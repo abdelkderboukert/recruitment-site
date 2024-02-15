1a480fdb2b92b16f237b960703522ce896f700e2
abdelkader <abdelkaderboukart@gmail.com>
2024-02-15 21:20:34 +0100

    (generated by blackbox.ai )

--- ecom/web/models.py
+++ ecom/web/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
-# Create your models here.
+class user(models.Model):
+    first_name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=40)
+    password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+    
+
+class info(models.Model):
+    use = models.ForeignKey(user, on_delete=models.CASCADE)
+    last_name = models.CharField(max_length=20)
+    age = models.CharField(max_length=2)
+    address = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    image = models.ImageField(upload_to='uploads/info/')
+    date = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.last_name
