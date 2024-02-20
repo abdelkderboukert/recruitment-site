5d3c93400a77e272986649332a11f824af00f803
abdelkader <abdelkaderboukart@gmail.com>
2024-02-20 01:01:54 +0100

    sing up

---
+++ ecom/static/js/register.js
@@ -0,0 +1,66 @@
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+if (navigator.userAgent.indexOf("Chrome") > -1 || navigator.userAgent.indexOf("Safari") > -1) {
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = 'body::-webkit-scrollbar { display: none; }';
+    document.getElementsByTagName('head')[0].appendChild(style);
+  } else if (navigator.userAgent.indexOf("MSIE") > -1 || navigator.userAgent.indexOf("Trident") > -1) {
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = 'body { -ms-overflow-style: none; }';
+    document.getElementsByTagName('head')[0].appendChild(style);
+  }
+
+  document.getElementById('btn-email').addEventListener('input', function() {
+    this.style.borderColor = '#64a19d';
+  });
+
+  document.getElementById('btn-password').addEventListener('input', function() {
+    this.style.borderColor = '#64a19d';
+  });
