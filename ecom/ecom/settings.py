f3fac2dc87110aaa043b3fe111d3b9dfaa83589a
abdelkader <abdelkaderboukart@gmail.com>
2024-02-27 08:44:13 +0100

    ok

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -126,3 +126,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000'
+]
